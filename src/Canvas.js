50a2e4f4e44b4ac7da23e68026e511e3c7a37a94
Kevin Huayllas <KevinOmega01@gmail.com>
2023-07-10 16:03:42 -0400

    add tooltip

--- src/Canvas.js
+++ src/Canvas.js
@@ -17,21 +17,39 @@ const Canvas = () => {
     xml.send();
     xml.onload = () => {
       const json = JSON.parse(xml.responseText);
-      setData(json.data);
+      setData(
+        json.data.map((d) => {
+          const year = Number(d[0].substring(0, 4));
+          const month = d[0].substring(5, 7);
+          let quarter = "Q1";
+          if (month === "04") {
+            quarter = "Q2";
+          } else if (month === "07") {
+            quarter = "Q3";
+          } else if (month === "10") {
+            quarter = "Q4";
+          }
+
+          return [year, d[1], quarter];
+        })
+      );
     };
   }, []);
 
   useEffect(() => {
     if (data.length && !drawed) {
+      console.log(data);
       setDrawed(true);
       const canvasWidth = canvasRef.current.getBoundingClientRect().width;
       const canvasHeight = canvasRef.current.getBoundingClientRect().height;
-      const padding = 100;
+      const padding = 70;
+      const barWidth = (canvasWidth - padding) / data.length;
+
       const xScale = d3
         .scaleLinear()
         .domain([
-          d3.min(data, (d) => Number(d[0].match(/\d{4}/)[0])),
-          d3.max(data, (d) => Number(d[0].match(/\d{4}/)[0])),
+          d3.min(data, (d) => Number(d[0])),
+          d3.max(data, (d) => Number(d[0])),
         ])
         .range([padding, canvasWidth - padding]);
       const yScale = d3
@@ -41,6 +59,12 @@ const Canvas = () => {
 
       const svg = d3.select("#canvas");
 
+      const tooltip = d3
+        .select("#canvas-container")
+        .append("div")
+        .attr("id", "tooltip");
+      // .style("opacity", 0);
+      console.log(tooltip);
       svg
         .selectAll("rect")
         .data(data)
@@ -49,9 +73,8 @@ const Canvas = () => {
         .attr("class", "bar")
         .attr("data-date", (d) => d[0])
         .attr("data-gpd", (d) => d[1])
-        .attr("width", (canvasWidth - padding) / data.length)
+        .attr("width", barWidth)
         .attr("height", (d) => {
-          console.log(yScale(d[1]), d[1]);
           return canvasHeight - yScale(d[1]) - padding;
         })
         .attr("fill", "navy")
@@ -59,7 +82,20 @@ const Canvas = () => {
           "x",
           (d, i) => i * ((canvasWidth - padding * 2) / data.length) + padding
         )
-        .attr("y", (d) => yScale(d[1]));
+        .attr("y", (d) => yScale(d[1]))
+        .on("mouseover", (e, d) => {
+          tooltip
+            .html(
+              d[0] +
+                "<br>" +
+                "$" +
+                d[1].toFixed(1).replace(/(\d)(?=(\d{3})+\.)/g, "$1,") +
+                " Billion"
+            )
+            .attr("data-date", d[0])
+            .style("left", data.indexOf(d) * barWidth + padding  + "px")
+            .style("transform", "translateX(60px)");
+        });
 
       const xAxis = d3.axisBottom(xScale);
       const yAxis = d3.axisLeft(yScale);
@@ -79,7 +115,11 @@ const Canvas = () => {
     }
   }, [data, drawed]);
 
-  return <svg id="canvas" ref={canvasRef}></svg>;
+  return (
+    <div id="canvas-container">
+      <svg id="canvas" ref={canvasRef}></svg>
+    </div>
+  );
 };
 
 export default Canvas;
