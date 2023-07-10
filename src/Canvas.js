970d5b47a0aa1c25be4634ab7fdfc8d645433d80
Kevin Huayllas <KevinOmega01@gmail.com>
2023-07-10 12:19:12 -0400

    add axix

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,8 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./canvas.css";
+import * as d3 from "d3";
 
 const Canvas = () => {
-  const [data, setData] = useState();
+  const [data, setData] = useState([]);
+  const canvasRef = useRef();
+  const [drawed, setDrawed] = useState(false);
 
   useEffect(() => {
     const xml = new XMLHttpRequest();
@@ -19,10 +22,57 @@ const Canvas = () => {
   }, []);
 
   useEffect(() => {
-    console.log(data);
-  }, [data]);
+    if (data.length && !drawed) {
+      setDrawed(true);
+      const canvasWidth = canvasRef.current.getBoundingClientRect().width;
+      const canvasHeight = canvasRef.current.getBoundingClientRect().height;
+      const padding = 100;
+      const xScale = d3
+        .scaleLinear()
+        .domain([
+          d3.min(data, (d) => Number(d[0].match(/\d{4}/)[0])),
+          d3.max(data, (d) => Number(d[0].match(/\d{4}/)[0])),
+        ])
+        .range([padding, canvasWidth - padding]);
+      const yScale = d3
+        .scaleLinear()
+        .domain([0, d3.max(data, (d) => d[1])])
+        .range([canvasHeight - padding, padding]);
 
-  return <div id="canvas"></div>;
+      const svg = d3.select("#canvas");
+
+      svg
+        .selectAll("rect")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("width", (canvasWidth - padding) / data.length)
+        .attr("height", (d) => {
+          console.log(yScale(d[1]), d[1]);
+          return canvasHeight - yScale(d[1]) - padding;
+        })
+        .attr("fill", "navy")
+        .attr(
+          "x",
+          (d, i) => i * ((canvasWidth - padding * 2) / data.length) + padding
+        )
+        .attr("y", (d) => yScale(d[1]));
+
+      const xAxis = d3.axisBottom(xScale);
+      const yAxis = d3.axisLeft(yScale);
+
+      svg
+        .append("g")
+        .style("transform", `translateY(${canvasHeight - padding + 10}px)`)
+        .call(xAxis);
+      svg
+        .append("g")
+        .style("transform", `translateX(${padding - 10}px)`)
+        .call(yAxis);
+    }
+  }, [data, drawed]);
+
+  return <svg id="canvas" ref={canvasRef}></svg>;
 };
 
 export default Canvas;
